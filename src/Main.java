d33ec7a50df02c4d715ae0525871d774d314c58d
Israel <icaroisrael@gmail.com>
2020-03-02 22:44:30 -0300

    Criação do método de transferencia

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import jdk.nashorn.internal.scripts.JO;
+
 import javax.swing.*;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Conta c1 = new Conta();
+        boolean prop = false;
+        boolean dest = false;
+        ArrayList<Conta> contas = new ArrayList<Conta>();
         int op = 10;
         while(op != 0){
             op = Integer.parseInt(
@@ -10,26 +16,48 @@ public class Main {
                             "Digite sua opção \n" +
                                     "1 - Nova Conta\n" +
                                     "2 - Saldo\n" +
-                                    "3 - depositp\n"+
-                                    "4 - saque\n" +
+                                    "3 - Deposito\n"+
+                                    "4 - Saque\n" +
+                                    "5 - Transferência"+
                                     "0 - Sair"));
             if(op == 1){
+                Conta c1 = new Conta();
                 c1.nome = JOptionPane.showInputDialog(
                         "Digite o nome do propietário");
+                contas.add(c1);
             }else if(op == 2){
-                System.out.println(
-                        "Nome = " + c1.nome +
-                        "Saldo = " + c1.getSaldo());
-            }else if(op == 3){
-                double valor = Double.parseDouble(
-                        JOptionPane.showInputDialog(
-                       "Digite o valor do deposito"));
-                c1.deposito(valor);
+                for(Conta conta: contas){
+                    System.out.println("Nome" + conta.nome + "\nSaldo = " + conta.saldo);
+                }
+            }else if(op == 5){
+                String proprietario = JOptionPane.showInputDialog("Digite o nome do proprietário");
+                for (Conta conta : contas){
+                    if (conta.nome.equals(proprietario)) {
+                        prop = true;
+                        String destinatario = JOptionPane.showInputDialog("Digite o nome do destinatário");
+                        for (Conta contaDestinatario : contas){
+                            if (contaDestinatario.nome.equals(destinatario)){
+                                dest = true;
+                                double valor = Double.parseDouble( JOptionPane.showInputDialog("Digite o valor da transferência"));
+                                conta.saldo = conta.saldo - valor;
+                                contaDestinatario.saldo = contaDestinatario.saldo + valor;
+                            }
+                        }
+                    }
+                    }
+                if(prop == false){
+                    JOptionPane.showMessageDialog(null, "Proprietário não existe");
+                }
+                if(dest == false){
+                    JOptionPane.showMessageDialog(null, "Destinatário não existe");
+                }
+
+
             }else if(op == 4){
                 double valor = Double.parseDouble(
                         JOptionPane.showInputDialog(
                                 "Digite o valor do saque"));
-                c1.saque(valor);
+                //c1.saque(valor);
 
             }
         }
